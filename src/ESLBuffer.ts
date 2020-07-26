4a7043bc4e3670b4ae3c04819cdd3c43427402a3
IvanPenga <ivan.penga@briteback.com>
2020-07-26 11:05:11 +0200

    increase parser speed by double

--- src/ESLBuffer.ts
+++ src/ESLBuffer.ts
@@ -3,6 +3,8 @@ import { ESLResponse } from './interfaces';
 class ESLBuffer {
 
     buffer: Buffer;
+    chunks: Buffer[] = [];
+    chunksTotalLength: number = 0;
 
     constructor(){
         this.buffer = Buffer.alloc(0);
@@ -11,9 +13,18 @@ class ESLBuffer {
     empty() {
         this.buffer = Buffer.alloc(0);
     }
-
+    
     getResponses(data: Buffer){
-        this.buffer = Buffer.concat([this.buffer, data], this.buffer.length + data.length);
+
+        this.chunks.push(data);
+        this.chunksTotalLength += data.length;
+
+        if (data.indexOf('\n\n') === -1) return [];
+        this.buffer = Buffer.concat([this.buffer, ...this.chunks], this.chunksTotalLength + this.buffer.length);
+
+        this.chunks = [];
+        this.chunksTotalLength = 0;
+
         const responses = [];
         let response = this.getNextResponse();
         while (response) {
@@ -31,6 +42,7 @@ class ESLBuffer {
         }
 
         const rawHeaders = buffer.slice(0, endOfResponse).toString();
+
         const response: ESLResponse = {  
             headers: { 
                 ['Content-Length']: 0, 
